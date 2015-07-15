af19680233de4597237a2d8f09db86f82214e9fe
Ubuntu <ubuntu@spark-master.novalocal>
2015-07-15 16:43:06 -0500

    version of aps_threshold.py that calculates average of all input images

--- code/spark/aps_threshold.py
+++ code/spark/aps_threshold.py
@@ -9,20 +9,27 @@ from __future__ import print_function
 from __future__ import division
 import sys
 import numpy as np
-from skimage import data,filter
+from skimage import io,data,filters
 from pyspark import SparkContext
-from libtiff import TIFF
+#from libtiff import TIFF
+from glob import glob
 import re
+#from sys import stdout
+from PIL import Image 
 
 def readTiff(name):
     """returns a tuple of the slice index and loaded tiff"""
-    print("Reading %s..."%name)
-    num=re.match("image(...).tif",name)  #yeah, yeah...
-    tif=TIFF.open(name)
-    return (num,tif,1)
+    num=int(re.match(".*image(...).tif",name).group(1))  #yeah, yeah...
+    print("Reading %s, num = %d..."%(name,num))
+    #tif=TIFF.open(name)     # read image info using libtiff (don't worry, this only loads the metadata, very fast)
+    #print(tif.info())
+    img=io.imread(name)/65535 # read image data using skimage.io (because libtiff.read_image doesn't work!)
+    print("image range: "+str(img.min())+" to "+str(img.max()))
+    return ([num],img,1)  # (slice, data, running_average_count)
 
 def imgavg(a,b):
-    newidx=a[0] #arbitrary
+    print("reducing...")
+    newidx=a[0]; newidx.extend(b[0])
     avgimg=(a[1]*a[2]+b[1]*b[2])/(a[2]+b[2]) #running average
     newden=a[2]+b[2]
     return (newidx,avgimg,newden)
@@ -31,8 +38,9 @@ def savebin(x):
     basedir='/tmp'  #'/mnt'
     idx=len(glob(basedir+'/avg_output-*.tif'))
     outfilename=basedir+"/avg_output-"+str(idx).zfill(2)+".tif"
-    outfile=TIFF.open(outfilename,mode='w')
-    outfile.write_image(x[1])
+    print("saving %s..."%outfilename)
+    result=Image.fromarray(x[1])
+    result.save(outfilename)
 
 if __name__ == "__main__":
 
@@ -42,13 +50,10 @@ if __name__ == "__main__":
 
     sc = SparkContext(appName="APS_Thresholder")
 
-    filelist=sc.textFile(sys.argv[1])
+    filelist=sc.textFile(sys.argv[1],30)
     stack=filelist.map(readTiff).cache()
-    
     print("numPartitions(%d,%s): %d"%(stack.id(),stack.name(),stack.getNumPartitions()))
-
     avg = stack.reduce(imgavg)
-
     savebin(avg)
 
     sc.stop()
