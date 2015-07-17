216a757acced74017755c4021d8c91b29abd0531
Cameron Christensen <camc@cc087.mcp.alcf.anl.gov>
2015-07-17 22:11:11 +0000

    fix simple_map.py

--- code/spark/simple_map.py
+++ code/spark/simple_map.py
@@ -12,28 +12,28 @@ import numpy as np
 from pyspark import SparkContext
 from glob import glob
 
-def parseVectors(arr):
-    arr= np.fromstring(arr,dtype=np.float64)
-    arr.reshape(arr.shape[0]/3,3)
-    return arr
+def parseVectors(bin):
+    arr= np.fromstring(bin[1],dtype=np.float64)
+    arr=arr.reshape(arr.shape[0]/3,3)
+    return (bin[0],arr)
 
 def add_vec3(arr,vec):
-    for v in arr:
-        v[0] += vec[0]
-        v[1] += vec[1]
-        v[2] += vec[2]
+    for i in xrange(len(arr[1])):
+        arr[1][i][0] += vec[0]
+        arr[1][i][1] += vec[1]
+        arr[1][i][2] += vec[2]
     return arr
 
 def construct_apply_shift(shift):
     return lambda p: add_vec3(p,shift)
         
-def savebin(iterator):
-    basedir='/tmp'  #'/mnt'
-    idx=len(glob(basedir+'/binary_output*.bin'))
-    outfilename=basedir+"/binary_output-"+str(idx).zfill(2)+".bin"
+def savebin(arr):
+    import re
+    basedir='/projects/visualization/cam/output'
+    idx=re.match(".*(.-..?)\.bin",arr[0]).group(1) # terribly specific
+    outfilename=basedir+"/binary_output-"+str(idx)+".bin"
     outfile=open(outfilename,'w')
-    for x in iterator:
-        outfile.write(x.data)
+    outfile.write(arr[1].data)
 
 if __name__ == "__main__":
 
@@ -50,6 +50,6 @@ if __name__ == "__main__":
     shift=np.array([25.25,-12.125,6.333],dtype=np.float64)
     B = A.map(construct_apply_shift(shift))
     print("numPartitions(%d,%s): %d"%(B.id(),B.name(),B.getNumPartitions()))
-    B.foreachPartition(savebin)
+    B.foreach(savebin)
 
     sc.stop()
