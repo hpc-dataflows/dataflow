f1befe4f74297569ce2495821879a45afc7bd662
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-08-06 17:48:49 -0500

    some initial progress porting SimpleMap (from Python)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- code/spark-scala/SimpleMap/src/main/scala/SimpleMap.scala
+++ code/spark-scala/SimpleMap/src/main/scala/SimpleMap.scala
@@ -1,7 +1,13 @@
+import java.nio.file.Paths
 
-class SimpleMap {
+import org.apache.spark.SparkContext
+import java.io._
 
-  case class Config(src: String = "", dst: String = "",
+import scala.util.Try
+
+object SimpleMap {
+
+  case class Config(src: Option[String] = None, dst: Option[String] = None,
                     blocks: Int = 0, blockSize: Int = 0, nparts : Int = 1,
                     size: Int = 1, nodes: Int = 1)
 
@@ -12,14 +18,14 @@ class SimpleMap {
     (t1 - t0, result)
   }
 
-  def parseCommandLine(args: Array[String]) = {
+  def parseCommandLine(args: Array[String]) : Option[Config] = {
     val parser = new scopt.OptionParser[Config]("scopt") {
-      head("scopt", "3.x")
+      head("SimpleMap", "0.1.x")
       opt[String]('s', "src") action { (x, c) =>
-        c.copy(src = x)
+        c.copy(src = Some(x))
       } text ("s/src is a String property")
       opt[String]('d', "dst") action { (x, c) =>
-        c.copy(dst = x)
+        c.copy(dst = Some(x))
       } text ("d/dst is a String property")
       opt[Int]('b', "blocks") action { (x, c) =>
         c.copy(blocks = x)
@@ -36,15 +42,48 @@ class SimpleMap {
       opt[Int]('z', "size") action { (x, c) =>
         c.copy(size = x)
       } text ("p/nparts is a String property")
+      help("help") text("prints this usage text")
+
     }
     // parser.parse returns Option[C]
     parser.parse(args, Config()) map { config =>
       config
-    } getOrElse {
-      None
     }
   }
+
+  def createResultsDir(dst: String, resultsDirName: String): Boolean = {
+    val outdir = new File(dst, resultsDirName)
+    Try{ outdir.mkdirs() } getOrElse(false)
+  }
+
+  def generate(x : Int, blockCount : Int) = {
+    val seed = System.nanoTime() / (x+1)
+    //np.random.seed(seed)
+    print(s"($x) generating $blockCount vectors...")
+    val a = -1000
+    val b = 1000
+    val r = new scala.util.Random(seed)
+    val arr = Array.fill(blockCount, 3)(r.nextDouble)
+    (x,arr)
+  }
+
   def main(args: Array[String]) {
-    val config = parseCommandLine(args)
+    val config = parseCommandLine(args).getOrElse(Config())
+    val sc = new SparkContext()
+
+    createResultsDir(config.dst.getOrElse("."), "/results")
+
+    if (config.blocks > 0 && config.blockSize > 0) {
+      val rdd = sc.parallelize(0 to config.blocks, config.nodes * 12 * config.nparts)
+      val gen_block_count = (config.blockSize * 1E6 / 24).toInt // 24 bytes per vector
+      println(s"generating ${config.blocks} of $gen_block_count vectors each...")
+      //outfile.write("generating data...\n")
+      //outfile.write("partition_multiplier: "+str(args.nparts)+"\n")
+      //outfile.write("gen_num_blocks: "+str(gen_num_blocks)+"\n")
+      //outfile.write("gen_block_size: "+str(gen_block_size)+"\n")
+      //outfile.write("total_data_size: "+str(gen_num_blocks*gen_block_size)+"\n")
+      val A = rdd.map(x => generate(x, gen_block_count))
+    }
+    println(config)
   }
 }
